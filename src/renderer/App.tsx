78c3752c1c6dac6bce6372b3bfbae593fa6ca925
Thive-N <35178093+Thive-N@users.noreply.github.com>
2024-01-14 23:33:42 +0000

    Change: Moving around elements

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,26 +1,12 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Timer from './components/Timer/Timer';
-import ScoreCard from './components/ScoreCard/ScoreCard';
-
-function Root() {
-  return (
-    <div className="main-page">
-      <div className="score-sheet">
-        <ScoreCard />
-      </div>
-      <div className="timer">
-        <Timer />
-      </div>
-    </div>
-  );
-}
+import TimerPage from './pages/TimerPage/TimerPage';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Root />} />
+        <Route path="/" element={<TimerPage />} />
       </Routes>
     </Router>
   );
---
+++ src/renderer/pages/TimerPage/TimerPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './TimerPage.css';
+import Timer from '../../components/Timer/Timer';
+import ScoreCard from '../../components/ScoreCard/ScoreCard';
+
+function TimerPage() {
+  return (
+    <div className="main-page">
+      <div className="score-sheet">
+        <ScoreCard />
+      </div>
+      <div className="timer">
+        <Timer />
+      </div>
+    </div>
+  );
+}
+
+export default TimerPage;
