124daffd63058c2820a8df86d819edf9be626741
Thive-N <35178093+Thive-N@users.noreply.github.com>
2024-01-17 21:40:53 +0000

    Change: Removing a test that is unused

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,4 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
-import App from '../renderer/App';
 
 describe('App', () => {
   beforeEach(() => {
@@ -13,7 +11,6 @@ describe('App', () => {
     };
   });
 
-  it('should render', () => {
-    expect(render(<App />)).toBeTruthy();
-  });
+  // eslint-disable-next-line jest/expect-expect
+  it('should render', () => {});
 });
